9488c7284324d7f58a83685a74356cb4c93c09c2
Anurag Gupta <aklg826@gmail.com>
2020-08-06 10:00:18 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class QuizReact extends Component{
+ state = {
+ 	questionBank: [],
+ 	res: 0,
+ 	score: 0
+ 	
+ };
+  getQuestions = () =>{
+  quizService().then(question=> {
+   this.setState({
+   questionBank: question
+   });
+  });
+  };
+  computeAnswer = (answer,correctAnswer) =>{
+  if(answer === correctAnswer)
+  {
+  this.setState({
+    score: this.state.score + 1
+    });
+  }
+  if(this.state.res < 5)
+  {
+  this.setState({
+  res: this.state.res + 1 
+  });
+  }
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  playAgain = () => {
+  this.getQuestions();
+  this.setState({
+  res:0,
+  score:0
+  });
+  };
+  componentDidMount(){
+   this.getQuestions();
+  }
+	render(){
+	return (
+	<div className="container">
+	<div className="title">Fun Quiz by React</div>
+	{this.state.questionBank.length > 0 &&
+	this.state.res < 5 &&
+	 this.state.questionBank.map(
+	 ({question,answers,correct,questionId})=>(
+	 <QuestionBox question = {question} options = {answers}
+	 key = {questionId}
+	 selected={answer=>this.computeAnswer(answer,correct)}
+	 />)
+	)}
+	{ this.state.res === 5 ?( <Result score = {this.state.score} playAgain={this.playAgain}/> ):null}
+	</div>
+	);
+	}
+}
+
+ReactDOM.render(<QuizReact/>, document.getElementById("root"));
